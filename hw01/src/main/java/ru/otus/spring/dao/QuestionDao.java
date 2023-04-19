0379f1b31611f6e56f80c55636e109ebfce209d4
Anastasia <nastena_024@mail.ru>
2023-04-19 14:28:08 +0300

    mini fix

--- hw01/src/main/java/ru/otus/spring/dao/QuestionDao.java
+++ hw01/src/main/java/ru/otus/spring/dao/QuestionDao.java
@@ -1,10 +1,8 @@
 package ru.otus.spring.dao;
 
 import com.opencsv.CSVReader;
-import ru.otus.spring.domain.Question;
 
 import java.io.IOException;
-import java.util.List;
 
 public interface QuestionDao{
 
--- hw01/src/main/java/ru/otus/spring/dao/QuestionDaoImpl.java
+++ hw01/src/main/java/ru/otus/spring/dao/QuestionDaoImpl.java
@@ -3,12 +3,9 @@ package ru.otus.spring.dao;
 import com.opencsv.CSVParserBuilder;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
-import ru.otus.spring.domain.Question;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 public class QuestionDaoImpl implements QuestionDao{
 
@@ -20,7 +17,6 @@ public class QuestionDaoImpl implements QuestionDao{
 
     @Override
     public CSVReader findQuestion(){
-        List<Question> questions = new ArrayList<>();
         InputStream isStream = getClass().getClassLoader().getResourceAsStream(fileCsv);
         CSVReader reader = null;
         if (isStream != null) {
