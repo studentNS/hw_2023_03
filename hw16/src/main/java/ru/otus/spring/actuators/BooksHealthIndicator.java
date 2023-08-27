f9c96cfd2c2c5f0fcc465aee45b922a3fd8e77cb
Anastasia <nastena_024@mail.ru>
2023-08-27 18:53:32 +0300

    fix hw16

--- hw16/src/main/java/ru/otus/spring/actuators/BooksHealthIndicator.java
+++ hw16/src/main/java/ru/otus/spring/actuators/BooksHealthIndicator.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.actuate.health.Status;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import ru.otus.spring.repository.BookRepository;
 
@@ -11,17 +12,19 @@ import ru.otus.spring.repository.BookRepository;
 @Component
 public class BooksHealthIndicator  implements HealthIndicator {
 
-    private final BookRepository bookRepository;
+    private final JdbcTemplate jdbcTemplate;
 
     @Override
     public Health health() {
-        if (bookRepository.findAll().isEmpty()) {
-            return Health.down()
-                    .status(Status.DOWN)
-                    .withDetail("message", "No books")
+        try {
+            jdbcTemplate.execute("select 1");
+            return Health.up()
+                    .withDetail("message", "Status connection to database UP")
                     .build();
-        } else {
-            return Health.up().build();
+        } catch (Throwable t) {
+            return Health.down()
+                    .withDetail("message", "Status connection to database DOWN")
+                    .build();
         }
     }
 }
